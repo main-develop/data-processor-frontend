8f99a7a4f01ac8108a77a85bdba38c2f31a1d045
main-develop <main.develop@protonmail.com>
2025-06-01 17:56:43 +0200

    chore: Reorganize project structure, remove unused files

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-// import App from './App.tsx'
-import App from "./App_test";
+import "./globals.css";
+import App from "./components/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
